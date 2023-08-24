13dc9fc5d7a275403f5e82179559ff252c9d773b
lucas <42370824+LucasGini@users.noreply.github.com>
2023-08-24 23:52:31 +0800

    Update all_ usage.py

--- all_ usage.py
+++ all_ usage.py
@@ -1,1 +1,15 @@
-
+# 列表中所有元素都为True
+lst1 = [True, True, True]
+print(all(lst1))  # 输出: True
+ 
+# 列表中存在一个元素为False
+lst2 = [True, False, True]
+print(all(lst2))  # 输出: False
+ 
+# 字符串中所有字符都为True
+str1 = 'Hello'
+print(all(str1))  # 输出: True
+ 
+# 字符串中存在一个字符为False
+str2 = 'Hello0'
+print(all(str2))  # 输出: False
